6f4329ed1fca3fd12aac04f1d38ebc0f1feb8895
Michael Miranda <m.miranda1219@gmail.com>
2019-10-26 19:38:35 -0400

    chore: set content type header

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site 🔥</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site &#128293;</h1>")
 }
 
 func main() {
