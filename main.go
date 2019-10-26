8ac830e812d7e9f6dd54e8c5581e653e00425a61
Michael Miranda <m.miranda1219@gmail.com>
2019-10-26 19:58:20 -0400

    feat: add 404 page

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site &#128293;</h1>")
 	} else if path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you were looking for &#128546;</h1>")
 	}
 }
 
