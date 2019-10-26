12a53afb58b13dc9d2429ca4a1b77b64a0393dfb
Michael Miranda <m.miranda1219@gmail.com>
2019-10-26 19:50:38 -0400

    feat: add contact page

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site &#128293;</h1>")
+	path := r.URL.Path
+	if path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site &#128293;</h1>")
+	} else if path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	}
 }
 
 func main() {
