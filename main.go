50986662e5168d61b81bb6d34de618dbf16569ad
Michael Miranda <m.miranda1219@gmail.com>
2019-10-26 21:09:14 -0400

    create faq route

--- main.go
+++ main.go
@@ -17,9 +17,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
+// Exercise 1 - make faq route
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1 style=\"color:red\">This is the faq page</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":8000", r)
 }
