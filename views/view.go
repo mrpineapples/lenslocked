301f39fb8ee71cc9afd640904cfcd68a7b8026f7
Michael Miranda <m.miranda1219@gmail.com>
2019-10-30 01:18:01 -0400

    chore: add render method and refactor handlers

--- views/view.go
+++ views/view.go
@@ -2,12 +2,14 @@ package views
 
 import (
 	"html/template"
+	"net/http"
 	"path/filepath"
 )
 
-var LayoutDir = "views/layouts/"
-var TemplateExt = ".gohtml"
+var layoutDir = "views/layouts/"
+var templateExt = ".gohtml"
 
+// NewView creates a View object with a template and a layout
 func NewView(layout string, files ...string) *View {
 	files = append(files, layoutFiles()...)
 
@@ -21,13 +23,19 @@ func NewView(layout string, files ...string) *View {
 	}
 }
 
+// View represents an HTML view for a route
 type View struct {
 	Template *template.Template
 	Layout   string
 }
 
+// Render is used to render the view with the predefined layout
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
 func layoutFiles() []string {
-	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	files, err := filepath.Glob(layoutDir + "*" + templateExt)
 	if err != nil {
 		panic(err)
 	}
