c897d79536a771de46f4b5946b39e4fb72dccbe0
Michael Miranda <m.miranda1219@gmail.com>
2019-11-02 16:04:50 -0400

    chore: basic parse of signup form

--- controllers/users.go
+++ controllers/users.go
@@ -31,5 +31,9 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 // Create is used to process the signup form and creates a new account
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "This is a temporary response.")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
 }
