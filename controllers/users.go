72fc187c5752a014a8cb4243eae5fb79f612f612
Michael Miranda <m.miranda1219@gmail.com>
2019-11-02 15:04:53 -0400

    feat: support POST on signup

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/mrpineapples/lenslocked/views"
@@ -19,8 +20,16 @@ type Users struct {
 	NewView *views.View
 }
 
+// New is used to render the form where a user can create a new account
+// GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// Create is used to process the signup form and creates a new account
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a temporary response.")
 }
