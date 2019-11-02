5d185bfbfc3c32d188fbc84340315ee721005f2c
Michael Miranda <m.miranda1219@gmail.com>
2019-11-02 17:06:31 -0400

    chore: parse forms with gorilla schema

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"github.com/mrpineapples/lenslocked/views"
 )
 
@@ -28,12 +29,22 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 // Create is used to process the signup form and creates a new account
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostForm["password"])
+
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mrpineapples/lenslocked
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/gorilla/schema v1.1.0
+)
