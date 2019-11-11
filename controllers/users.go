d8c999fb5822346008f75fdce17ae18be430873b
Michael Miranda <m.miranda1219@gmail.com>
2019-11-10 21:05:47 -0500

    chore: connect the users' controller and model

--- controllers/users.go
+++ controllers/users.go
@@ -4,20 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/mrpineapples/lenslocked/models"
 	"github.com/mrpineapples/lenslocked/views"
 )
 
 // NewUsers is used to create a new Users controller.
 // It will panic if templates are not parsed correctly
 // and should only be used during setup
-func NewUsers() *Users {
+func NewUsers(us *models.UserService) *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "users/new"),
+		service: us,
 	}
 }
 
 type Users struct {
 	NewView *views.View
+	service *models.UserService
 }
 
 // New is used to render the form where a user can create a new account
@@ -29,6 +32,7 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 type SignupForm struct {
+	Name     string `schema:"name"`
 	Email    string `schema:"email"`
 	Password string `schema:"password"`
 }
@@ -39,6 +43,14 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
+	}
+	user := models.User{
+		Name:  form.Name,
+		Email: form.Email,
+	}
+	if err := u.service.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	fmt.Fprintln(w, form)
 }
--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/mrpineapples/lenslocked/controllers"
+	"github.com/mrpineapples/lenslocked/models"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "Michael"
+	password = "not-necessary"
+	dbname   = "lenslocked_dev"
 )
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.AutoMigrate()
+
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
@@ -17,10 +35,4 @@ func main() {
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":8000", r)
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
